10ebb933db2480481a8298f46cb10f0e95195384
Tealy <1131180370@qq.com>
2017-03-10 22:47:03 +0800

    category

---
+++ app/Http/Controllers/Admin/LoginControllere.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class LoginControllere extends Controller
+{
+    //
+    public function login()
+    {
+
+    }
+
+    //
+    public function quit()
+    {
+
+    }
+
+    //
+    public function code()
+    {
+
+    }
+}
